e8c49b1821e16439a38757f6f473c39f9359038d
Jacob Frederiksen <jac.frederiksen@gmail.com>
2022-07-26 09:19:00 +0200

    assasinator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import StartScreen from './pages/startscreen/StartScreen'
+import { Routes, Route } from "react-router-dom";
+import PlayerScreen from './pages/playerscreen/PlayerScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='app_con'>
+      <Routes>
+        <Route index element={<StartScreen/>}/>
+        <Route path="playerscreen" element={<PlayerScreen/>}/>
+      </Routes>
     </div>
+    
+   
   );
 }
 
---
+++ src/pages/playerscreen/PlayerScreen.js
@@ -0,0 +1,15 @@
+import styles from './playerscreen.module.scss'
+
+const PlayerScreen = () => {
+    return (
+
+        <>
+
+        <div className={styles.player_con}>Player</div>
+        
+        </>
+        
+    )
+}
+
+export default PlayerScreen;
