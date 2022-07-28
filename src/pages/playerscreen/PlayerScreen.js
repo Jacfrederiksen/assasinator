86ba42eeafbf8a70258705004a516e98e21091b0
Jacob Frederiksen <jac.frederiksen@gmail.com>
2022-07-28 10:06:22 +0200

    commit

--- src/pages/playerscreen/PlayerScreen.js
+++ src/pages/playerscreen/PlayerScreen.js
@@ -1,12 +1,28 @@
 import styles from './playerscreen.module.scss'
 
 const PlayerScreen = () => {
+
+
     return (
 
         <>
-
-        <div className={styles.player_con}>Player</div>
-        
+        <div className={styles.player_wrapper}>
+            <div className={styles.player_con}>
+                <header className={styles.player_head}>
+                    <h1>White</h1>
+                </header>
+                <section className={styles.player_sec}>
+                    <button>show target</button>
+                    <div className={styles.player_target_con}>
+                        <span className={styles.a_span}>assasinate</span>
+                        <span className={styles.player_target}><p>Target</p></span>
+                    </div>
+                </section>
+                <footer className={styles.player_foot}>
+                    <button>hide</button>
+                </footer>
+            </div>
+        </div>
         </>
         
     )
--- src/pages/startscreen/StartScreen.js
+++ src/pages/startscreen/StartScreen.js
@@ -8,18 +8,18 @@ const StartScreen = () => {
 
         <div className={styles.start_wrapper}>
             <div className={styles.start_con}>
-                <div className={styles.start_head}><h1>mtg <br></br> assasinator</h1></div>
-                <div className={styles.start_player_arrows}>
+                <header className={styles.start_head}><h1>mtg <br></br> assasinator</h1></header>
+                <section className={styles.start_player_arrows}>
                     <div className={styles.white_a}><img src='./assets/white_arrow.png'/></div>
                     <div className={styles.blue_a}><img src='./assets/blue_arrow.png'/></div>
                     <div className={styles.start_player_head}><h2>Players</h2></div>
                     <div className={styles.black_a}><img src='./assets/black_arrow.png'/></div>
                     <div className={styles.red_a}><img src='./assets/red_arrow.png'/></div>
                     <div className={styles.green_a}><img src='./assets/green_arrow.png'/></div>
-                </div>
-                <div className={styles.start_button_con}>
+                </section>
+                <footer className={styles.start_button_con}>
                     <Link to="/playerscreen"><button>Begin</button></Link>
-                </div>
+                </footer>
             </div>
         </div>
         
