4c73633a2c382312b2ef6e1baac9dab9c568e7d1
Jacob Frederiksen <jac.frederiksen@gmail.com>
2022-08-09 13:41:11 +0200

    Funktionerne virker

--- src/pages/playerscreen/PlayerScreen.js
+++ src/pages/playerscreen/PlayerScreen.js
@@ -5,40 +5,46 @@ import PassOverlay from '../../components/passOverlay/PassOverlay';
 
 const PlayerScreen = () => {
 
-    const [show, setShow] = useState(false);
-    const [count, setCount] = useState(0);
-    const [selected, setSelected] = useState([]);
-    const [targets, setTargets] = useState([]);
+    const [show, setShow] = useState(false);  //Portal
+    const [count, setCount] = useState(0);  //Counter til chooseTarget, bruges til at skifte data og styling til næste spiller
+    var [opponents, setOpponents] = useState([]); //Array med de shufflede elementer fra targets i data 
+    const [selected, setSelected] = useState([]);  //Array med de valgte targets fra det shufflede array
 
     
-    const chooseTarget = () => {
+    const chooseTarget = (e) => {
 
-        // Bruges til at skifte farve og tekst på playerscreen, dvs - vise næste spiller
-        setCount(count => count +1);
-
-
+    /*setCount(count => count +1); */
+        
         shuffle(data[count].target);
-
+        e.target.style.display = "none";
+        document.querySelector("#p_target_con").style.display="flex";
+        document.querySelector("#footer").style.display="flex";
     }
 
     const shuffle = () => {
 
-        setTargets(data[count].target.sort(() => Math.random() - 0.5))
+        console.log(count)
 
-        // Skal bruges til at pushe det valgte target ind i selected array
-        setSelected(selected => [...selected, targets[0]])
-
-        //setNames(current => [...current, 'Carl']);
-        console.log(selected)
-
-
+        setOpponents(data[count].target.sort(() => Math.random() - 0.5))
     }
 
+
     useEffect(() => {
+        if (!opponents.length) return;
+        setSelected(selected => [...selected, opponents[0]])
 
-        console.log(targets)
+        opponents = opponents.filter(remove => !selected.includes( remove ));
 
-    },[targets])
+        document.querySelector("#p_target_text").textContent=opponents[0];
+        document.querySelector("#p_target_child").style.backgroundColor=opponents[0];
+
+        console.log(opponents)
+        console.log(selected);
+        console.log(opponents[0]);
+
+        },[opponents])
+
+        //Filter, find eller reduce til at ungå gentagelser fra selected
 
     return (
 
@@ -50,12 +56,12 @@ const PlayerScreen = () => {
                 </header>
                 <section className={styles.player_sec}>
                     <button onClick={chooseTarget} style={{backgroundColor:data[count].but}}>show target</button>
-                    <div className={styles.player_target_con}>
+                    <div className={styles.player_target_con} id="p_target_con">
                         <span className={styles.a_span}>assasinate</span>
-                        <span className={styles.player_target}><p>{data[count].target}</p></span>
+                        <span className={styles.player_target} id="p_target_child"><p id="p_target_text">{data[count].target}</p></span>
                     </div>
                 </section>
-                <footer className={styles.player_foot}>
+                <footer className={styles.player_foot} id="footer">
                     <button style={{backgroundColor:data[count].but}} onClick={() => setShow((s) => !s)}>hide</button>
                 </footer>
             </div>
